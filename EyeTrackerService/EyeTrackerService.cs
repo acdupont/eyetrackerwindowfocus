44fca4fb388becc50ddeb6b3b8959dbffbfb7cdf
Anthony DuPont <acdupont@mtu.edu>
2016-04-10 12:17:57 -0400

    working code to install eyetracker service

---
+++ EyeTrackerService/EyeTrackerService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5421
+{
+    class EyeTrackerService : ServiceBase
+    {
+        public EyeTrackerService()
+        {
+            ServiceName = "EyeTrackerService";
+            CanStop = true;
+            CanPauseAndContinue = false;
+            AutoLog = true;
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            //base.OnStart(args);
+            Console.WriteLine("EyeTrackerService.OnStart");
+        }
+
+
+
+        protected override void OnStop()
+        {
+            //base.OnStop();
+            Console.WriteLine("EyeTrackerService.OnStop");
+        }
+
+
+        static void Main(string[] args)
+        {
+            System.ServiceProcess.ServiceBase.Run(new EyeTrackerService());
+        }
+
+        private void InitializeComponent()
+        {
+            // 
+            // EyeTrackerService
+            // 
+            this.ServiceName = "EyeTrackerService";
+
+        }
+    }
+}
---
+++ EyeTrackerService/EyeTrackerServiceInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5641
+{
+    [RunInstaller(true)]
+    public class EyeTrackerServiceInstaller : Installer
+    {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public EyeTrackerServiceInstaller()
+        {
+            processInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "EyeTrackerService";
+            serviceInstaller.Description = "Handles starting/stopping eyetracking service that integrates with window focusing";
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
